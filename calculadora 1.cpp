4d58b65615ea3c8370e664bfec8993a2976b8505
Alex7w7RM <90742335+Alex7w7RM@users.noreply.github.com>
2022-02-12 10:37:49 -0600

    Add files via upload

---
+++ calculadora 1.cpp
@@ -0,0 +1,31 @@
+#include "iostream"
+#include "string"
+using namespace std;
+int main()
+{
+	cout<< "Ingresar dos numeros" << "\n" ; 
+	float var1,var2; 
+	
+	cout<< "Ingresar el primer numero" << "\n";
+	cin>> var1; 
+	
+	cout<< "Ingresar el segundo numero"<< "\n"; 
+	cin>> var2; 
+	
+	cout<< "Que tipo de operacion desea hacer?" << "\n"; 
+	
+	cout<< " opcion 1: Suma" << "\n"; 
+	cout<< " Opcion 2: Resta" << "\n"; 
+	cout<< " Opcion 3: Multiplicacion" << "\n"; 
+	cout<< " Opcion 4: Division" << "\n";	
+	int opc1 ;
+	cout<< "ingrese un numero para seleccionar la opcion que usted desee ejecutar" << "\n"; 
+	cin>> opc1;
+	switch(opc1){ 
+		case 1: cout << var1 << " + " << var2 << " = " << var1 + var2; break; 
+		case 2: cout << var1 << " - " << var2 << " = " << var1 - var2; break;
+		case 3: cout << var1 << " * " << var2 << " = " << var1 * var2; break;
+		case 4: cout << var1 << " / " << var2 << " = " << var1 / var2; break;
+	}
+
+}
